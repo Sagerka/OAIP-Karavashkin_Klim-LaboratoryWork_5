856cdb55e909d0eec6d0d0f376b5c87e2d8facc6
Клим <nefsed56kek@gmail.com>
2026-02-02 17:36:20 +0300

    123

---
+++ LabWork5/LabWork5.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+class Interval {
+private:
+    int first;   // минимально знач
+    int second;  // максимальное знач
+
+public:
+    void Read() 
+    {
+        cout << "Введите first (начальное): ";
+        cin >> first;
+        cout << "Введите second (конечное): ";
+        cin >> second;
+    }
+
+    void Display() const 
+    {
+        cout << "Интервал: [" << first << ", " << second << ")\n";
+    }
+
+    bool rangecheck(int x) const 
+    {
+        return (x >= first && x < second);
+    }
+};
+
+int main() 
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Interval interval;
+    interval.Read();
+    interval.Display();
+
+    int x;
+    cout << "Введите число для проверки: ";
+    cin >> x;
+
+    if (interval.rangecheck(x)) 
+    {
+        cout << "Число " << x << " принадлежит интервалу\n";
+    }
+    else 
+    {
+        cout << "Число " << x << " не принадлежит интервалу\n";
+    }
+
+    return 0;
+}
